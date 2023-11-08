e4a16d42d1dba0893a2a47e800e3aa18edc55bcb
bhuvanesh-hj <bhuvaneshhj@gmail.com>
2023-11-09 00:31:59 +0530

    added the dynamic routes

--- routes/admin.js
+++ routes/admin.js
@@ -1,20 +1,18 @@
-// const path = require('path');
+const path = require('path');
 
 const express = require('express');
 
-// const rootDir = require('../util/path');
+const adminController = require('../controllers/admin');
 
 const router = express.Router();
 
-// const products = [];
-
-const productController = require('../controllers/product') 
-
 // /admin/add-product => GET
-router.get('/add-product', productController.getAddProduct );
+router.get('/add-product', adminController.getAddProduct);
+
+// /admin/products => GET
+router.get('/products', adminController.getProducts);
 
 // /admin/add-product => POST
-router.post('/add-product', productController.postAddproduct);
+router.post('/add-product', adminController.postAddProduct);
 
 module.exports = router;
-
--- routes/shop.js
+++ routes/shop.js
@@ -1,14 +1,21 @@
-// const path = require('path');
+const path = require('path');
 
 const express = require('express');
 
-// const rootDir = require('../util/path');
-// const adminData = require('./admin');
-
-const controllerProduct = require('../controllers/product')
+const shopController = require('../controllers/shop');
 
 const router = express.Router();
 
-router.get('/', controllerProduct.getProducts);
+router.get('/', shopController.getIndex);
+
+router.get('/products', shopController.getProducts);
+
+router.get('/products/:productId', shopController.getProduct);
+
+router.get('/cart', shopController.getCart);
+
+router.get('/orders', shopController.getOrders);
+
+router.get('/checkout', shopController.getCheckout);
 
 module.exports = router;
